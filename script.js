f5926c2dd56a633b286d1434b9834559e7b8076c
Divyansh Singh <singh.divyansh1802@gmail.com>
2023-02-22 18:01:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here
 
+let secretNumber = Math.floor(Math.random() * 100) + 1;
+      let previousGuess = null;
+      let previousDifference = null;
+
+      function checkGuess() {
+        const guess = parseInt(document.getElementById("guess").value);
+        if (isNaN(guess)) {
+          document.getElementById("response").innerHTML = "Please enter a valid number";
+        } else if (guess < 1 || guess > 100) {
+          document.getElementById("response").innerHTML = "Please enter a number between 1 and 100";
+        } else if (guess === secretNumber) {
+          document.getElementById("response").innerHTML = "Congratulations! You guessed the number";
+        } else {
+          const difference = Math.abs(guess - secretNumber);
+          if (previousGuess === null || difference < previousDifference) {
+            document.getElementById("response").innerHTML = "Getting hotter! Guess " + (guess < secretNumber ? "higher" : "lower");
+          } else {
+            document.getElementById("response").innerHTML = "Getting colder! Guess " + (guess < previousGuess ? "higher" : "lower");
+          }
+          previousGuess = guess;
+          previousDifference = difference;
+        }
+      }
