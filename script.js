2b49f8c8f0ccc4ed43eea87598d489aeb05d1d80
Divyansh Singh <singh.divyansh1802@gmail.com>
2023-02-27 11:21:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ let secretNumber = Math.floor(Math.random() * 100) + 1;
       function checkGuess() {
         const guess = parseInt(document.getElementById("guess").value);
         if (isNaN(guess)) {
-          document.getElementById("response").innerHTML = "Please enter a valid number";
+          document.getElementById("respond").innerHTML = "Please enter a valid number";
         } else if (guess < 1 || guess > 100) {
-          document.getElementById("response").innerHTML = "Please enter a number between 1 and 100";
+          document.getElementById("respond").innerHTML = "Please enter a number between 1 and 100";
         } else if (guess === secretNumber) {
-          document.getElementById("response").innerHTML = "Congratulations! You guessed the number";
+          document.getElementById("respond").innerHTML = "Congratulations! You guessed the number";
         } else {
           const difference = Math.abs(guess - secretNumber);
           if (previousGuess === null || difference < previousDifference) {
-            document.getElementById("response").innerHTML = "Getting hotter! Guess " + (guess < secretNumber ? "higher" : "lower");
+            document.getElementById("respond").innerHTML = "Getting hotter! Guess " + (guess < secretNumber ? "higher" : "lower");
           } else {
-            document.getElementById("response").innerHTML = "Getting colder! Guess " + (guess < previousGuess ? "higher" : "lower");
+            document.getElementById("respond").innerHTML = "Getting colder! Guess " + (guess < previousGuess ? "higher" : "lower");
           }
           previousGuess = guess;
           previousDifference = difference;
